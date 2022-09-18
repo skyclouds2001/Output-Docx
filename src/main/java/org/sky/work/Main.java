df1e7ae9e0fe7a60e94dfc07de533095ab87b2bc
skyclouds2001 <skyclouds2001@163.com>
2022-09-18 21:13:47 +0800

    feat: add test method

--- src/main/java/org/sky/work/Main.java
+++ src/main/java/org/sky/work/Main.java
@@ -2,5 +2,12 @@ package org.sky.work;
 
 public class Main {
     public static void main(String[] args) {
+        final ToDoc to = new ToDoc();
+
+        try {
+            to.test();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/org/sky/work/ToDoc.java
+++ src/main/java/org/sky/work/ToDoc.java
@@ -1,5 +1,138 @@
 package org.sky.work;
 
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.util.Units;
+import org.apache.poi.xwpf.model.XWPFHeaderFooterPolicy;
+import org.apache.poi.xwpf.usermodel.*;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.*;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ToDoc {
-    public void exportDoc () {}
+
+    public void exportDoc() {
+    }
+
+    public void test() throws IOException, InvalidFormatException {
+
+        XWPFDocument document = new XWPFDocument();
+        FileOutputStream out = new FileOutputStream("D:\\程序\\outputPDF\\dist\\eg.docx");
+
+        //添加标题
+        XWPFParagraph titleParagraph = document.createParagraph();
+        //设置段落居中
+        titleParagraph.setAlignment(ParagraphAlignment.CENTER);
+
+        XWPFRun titleParagraphRun = titleParagraph.createRun();
+
+        titleParagraphRun.setText("Java PoI");
+        titleParagraphRun.setColor("000000");
+        titleParagraphRun.setFontSize(20);
+        //段落
+        XWPFParagraph firstParagraph = document.createParagraph();
+        XWPFRun run = firstParagraph.createRun();
+        run.setText("Java POI 生成word文件。");
+        run.setColor("696969");
+        run.setFontSize(16);
+
+        //设置段落背景颜色
+        CTShd cTShd = run.getCTR().addNewRPr().addNewShd();
+        cTShd.setVal(STShd.CLEAR);
+        cTShd.setFill("97FFFF");
+
+        //换行
+        XWPFParagraph paragraph1 = document.createParagraph();
+        XWPFRun paragraphRun1 = paragraph1.createRun();
+        paragraphRun1.setText("\r");
+
+        //基本信息表格
+        XWPFTable infoTable = document.createTable();
+        //去表格边框
+        infoTable.getCTTbl().getTblPr().unsetTblBorders();
+
+        //列宽自动分割
+        CTTblWidth infoTableWidth = infoTable.getCTTbl().addNewTblPr().addNewTblW();
+        infoTableWidth.setType(STTblWidth.DXA);
+        infoTableWidth.setW(BigInteger.valueOf(9072));
+
+        //表格第一行
+        XWPFTableRow infoTableRowOne = infoTable.getRow(0);
+        infoTableRowOne.getCell(0).setText("职位");
+        infoTableRowOne.addNewTableCell().setText(": Java 开发工程师");
+
+        //表格第二行
+        XWPFTableRow infoTableRowTwo = infoTable.createRow();
+        infoTableRowTwo.getCell(0).setText("姓名");
+        infoTableRowTwo.getCell(1).setText(": seawater");
+
+        //表格第三行
+        XWPFTableRow infoTableRowThree = infoTable.createRow();
+        infoTableRowThree.getCell(0).setText("生日");
+        infoTableRowThree.getCell(1).setText(": xxx-xx-xx");
+
+        //表格第四行
+        XWPFTableRow infoTableRowFour = infoTable.createRow();
+        infoTableRowFour.getCell(0).setText("性别");
+        infoTableRowFour.getCell(1).setText(": 男");
+
+        //表格第五行
+        XWPFTableRow infoTableRowFive = infoTable.createRow();
+        infoTableRowFive.getCell(0).setText("现居地");
+        infoTableRowFive.getCell(1).setText(": xx");
+
+
+        CTSectPr sectPr = document.getDocument().getBody().addNewSectPr();
+        XWPFHeaderFooterPolicy policy = new XWPFHeaderFooterPolicy(document, sectPr);
+
+        XWPFParagraph pic = document.createParagraph();
+        pic.setAlignment(ParagraphAlignment.CENTER);
+        XWPFRun picRun = pic.createRun();
+        List<String> filePath = new ArrayList<>();
+        filePath.add("D:\\程序\\outputPDF\\dist\\repository-open-graph-template.png");
+        filePath.add("D:\\程序\\outputPDF\\dist\\repository-open-graph-template.png");
+        filePath.add("D:\\程序\\outputPDF\\dist\\repository-open-graph-template.png");
+        filePath.add("D:\\程序\\outputPDF\\dist\\repository-open-graph-template.png");
+
+        for (String str : filePath) {
+            picRun.setText(str);
+            picRun.addPicture(
+                    new FileInputStream(str), XWPFDocument.PICTURE_TYPE_JPEG,
+                    str,
+                    Units.toEMU(450),
+                    Units.toEMU(300)
+            );
+        }
+        //添加页眉
+        CTP ctpHeader = CTP.Factory.newInstance();
+        CTR ctrHeader = ctpHeader.addNewR();
+        CTText ctHeader = ctrHeader.addNewT();
+        String headerText = "ctpHeader";
+        ctHeader.setStringValue(headerText);
+        XWPFParagraph headerParagraph = new XWPFParagraph(ctpHeader, document);
+        //设置为右对齐
+        headerParagraph.setAlignment(ParagraphAlignment.RIGHT);
+        XWPFParagraph[] parsHeader = new XWPFParagraph[1];
+        parsHeader[0] = headerParagraph;
+        policy.createHeader(XWPFHeaderFooterPolicy.DEFAULT, parsHeader);
+
+        //添加页脚
+        CTP ctpFooter = CTP.Factory.newInstance();
+        CTR ctrFooter = ctpFooter.addNewR();
+        CTText ctFooter = ctrFooter.addNewT();
+        String footerText = "ctpFooter";
+        ctFooter.setStringValue(footerText);
+        XWPFParagraph footerParagraph = new XWPFParagraph(ctpFooter, document);
+        headerParagraph.setAlignment(ParagraphAlignment.CENTER);
+        XWPFParagraph[] parsFooter = new XWPFParagraph[1];
+        parsFooter[0] = footerParagraph;
+        policy.createFooter(XWPFHeaderFooterPolicy.DEFAULT, parsFooter);
+        document.write(out);
+        out.close();
+    }
+
 }
