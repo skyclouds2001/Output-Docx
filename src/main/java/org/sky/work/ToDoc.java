d2e8d7450d622035bdaf4a545a94b6e93884cf70
skyclouds2001 <skyclouds2001@163.com>
2022-09-20 14:09:28 +0800

    feat: add Base Export

--- src/main/java/org/sky/work/ToDoc.java
+++ src/main/java/org/sky/work/ToDoc.java
@@ -16,6 +16,66 @@ import java.util.List;
 public class ToDoc {
 
     public void exportDoc() throws IOException {
+
+        XWPFDocument document = new XWPFDocument();
+        FileOutputStream out = new FileOutputStream("D:\\程序\\outputPDF\\dist\\eg.docx");
+
+        // 创建标题
+        XWPFParagraph title = document.createParagraph();
+        title.setAlignment(ParagraphAlignment.CENTER);
+        XWPFRun titleRun = title.createRun();
+        titleRun.setText("检查问题汇总清单");
+        titleRun.setColor("000000");
+        titleRun.setFontSize(20);
+        titleRun.setBold(true);
+
+        // 创建表格
+        XWPFTable table = document.createTable();
+
+        // 初始化表格
+        {
+            XWPFTableRow row = table.getRow(0);
+            for (int i = 0; i < 8 - 1; ++i) {
+                row.addNewTableCell();
+            }
+            for (int i = 0; i < Data.records.size() - 1; ++i) {
+                table.createRow();
+            }
+            table.createRow();
+            table.createRow();
+            XWPFTableRow row1 = table.getRow(0);
+            XWPFTableRow row2 = table.getRow(1);
+            row1.getCell(0).setText("专业");
+            row1.getCell(1).setText("序号");
+            row1.getCell(2).setText("检查表中检查内容");
+            row1.getCell(3).setText("问题描述");
+            row1.getCell(4).setText("问题照片");
+            row1.getCell(5).setText("整改意见");
+            row1.getCell(6).setText("问题归属");
+            row2.getCell(6).setText("文件、资料类");
+            row2.getCell(7).setText("现场类");
+        }
+
+        // 插入数据
+        for (int i = 0; i < Data.records.size(); ++i) {
+            XWPFTableRow row = table.getRow(i + 2);
+            Record data = Data.records.get(i);
+            row.getCell(0).setText(data.type);
+            row.getCell(1).setText(String.valueOf(data.index));
+            row.getCell(2).setText(data.content);
+            row.getCell(3).setText(data.desc);
+            row.getCell(4).setText(data.imgURL);
+            row.getCell(5).setText(data.advice);
+            if (data.qType == 1) {
+                row.getCell(6).setText("√");
+            } else if (data.qType == 2) {
+                row.getCell(7).setText("√");
+            }
+        }
+
+        document.write(out);
+        out.close();
+
     }
 
     public void test() throws IOException, InvalidFormatException {
